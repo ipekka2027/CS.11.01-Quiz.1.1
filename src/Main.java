ba43415ec262db1ce94e80d2c8eb1b03ecac365e
techarenz <57818506+techarenz@users.noreply.github.com>
2024-08-27 12:35:11 +0800

    Update Main.java
    Included academic honesty pledge.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
+
+/**
+*****************************************************************************************************
+* By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
+* “I have neither given nor received unauthorized aid on this piece of work.”
+* NAME: Ipek
+*****************************************************************************************************
+*/
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+        System.out.println("Enter your first name: ");
+        scanner.nextLine();
+        firstName = scanner.nextLine();
+        System.out.println("Enter your favourite food: ");
+        favouriteFood = scanner.nextLine();
 
+        System.out.println("First Name: " + firstName + "\nAge" + age + "\nFavourite Food: " + favouriteFood);
 
     }
 
